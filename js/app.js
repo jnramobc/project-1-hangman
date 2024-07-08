66c799fbd4254a88a3260d5acad1423f45735f58
jnramobc <54511908+jnramobc@users.noreply.github.com>
2024-07-08 07:08:25 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,79 +1,99 @@
 /*-------------------------------- Constants --------------------------------*/
-const hangmanPics = [ "
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========", "
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========", "
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========","
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========", "
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========", "
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========","
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========" ]
+const wordsList = ["Babble","Babies","Beagle","Pacify","Wacked",
+                   "Yachts","Puzzle","Fizzed","Snazzy","Dazzle"]
 
-const wordsList = [Babble, Baby, eagles, pacify, wacked, yachts, puzzle, fizzed, snazzy, dazzle, ]
-
-
+const hangmanPictures = [`
+    +---+
+    |     |
+          |
+          |
+          |
+          |
+  =========`, `
+    +---+
+    |     |
+    O     |
+          |
+          |
+          |
+  =========`, `
+    +---+
+    |     |
+    O     |
+    |     |
+          |
+          |
+  =========`, `
+    +---+
+    |     |
+    O     |
+   /|     |
+          |
+          |
+  ========= `, `
+    +---+
+    |     |
+    O     |
+   /|\    |
+          |
+          |
+  =========`, `
+    +---+
+    |     |
+    O     |
+   /|\    |
+   /      |
+          |
+  ========= `, `
+    +---+
+    |     |
+    O     |
+   /|\    |
+   / \    |
+          |
+  ========= `]
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-let letterChosen;
-let letterLeft;
-turnsLet;
-
+let lettersLeft = 26;
+let turnsLeft= 6;
+let hangmanStatus = hangmanPictures[0];
+let letterChoice;
+let randomWord;
 /*------------------------ Cached Element References ------------------------*/
 
-
-
+const displayMessageEl = document.querySelector('#display-message');
+const alphaSelectorEl = document.querySelectorAll('.box');
+const spaceLinesEl = document.querySelectorAll('.line');
+const bodyElement = document.querySelector("body");
+const sectionEl = document.querySelector('.spaceman-lines')
+let divElement = document.querySelector('.hangmen')
 /*-------------------------------- Functions --------------------------------*/
+function pickWord(list) {
+  const idx = Math.floor(Math.random() * wordsList.length)
+  randomWord = list[idx]
+  return randomWord
+}
 
+pickWord(wordsList)
 
+divElement.textContent = hangmanPictures[0];
+sectionEl.before(divElement);
 
+const getLetterchoice = () => {
+    
+}
+
+const updateDisplay = () => {
+    if (turnsLeft >=1) 
+        displayMessageEl.textContent = "Choose a letter"
+}
+
+updateDisplay()
+let chooseLetter = () => {
+
+}
 /*----------------------------- Event Listeners -----------------------------*/
+document.querySelector('.box').addEventListener('click', chooseLetter)
 
 
-
-/*
-git add -A
-git commit -m "message describing code changes"
-git push origin main
-*/
